144eeaeda94954bf08063be9f2c3920b03f6ecc0
Yasmine Rico <ricolyasmine@gmail.com>
2022-05-17 14:35:55 -0500

    complete Dice Roll exercise

--- src/main/java/com/codeup/springblog/controllers/DiceRoll.java
+++ src/main/java/com/codeup/springblog/controllers/DiceRoll.java
@@ -1,27 +1,37 @@
 package com.codeup.springblog.controllers;
 
 import org.springframework.stereotype.Controller;
-
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class DiceRoll {
-
     @GetMapping("/dice-roll")
-    public String rollDice() {
-        return "dice-roll";
+    public String viewRollDice(){
+        return "view/dice-roll";
     }
 
-    @PostMapping("/dice-roll")
-    public String checkDice(@RequestParam int num, Model model) {
-        int random = (int) (Math.random() * 6 + 1);
-        model.addAttribute("num", num);
-        model.addAttribute("random", random);
-        return "dice-roll";
+    @GetMapping("/dice-roll/{n}")
+    public String viewResults(@PathVariable int n, Model model){
+
+        int diceRoll = (int) (Math.random() * 6) + 1;
+        String message;
+
+        if(diceRoll == n){
+            message = "You guessed correctly!";
+        }else{
+            message = "Incorrect. Try again";
+        }
+
+        model.addAttribute("n", n);
+        model.addAttribute("diceRoll", diceRoll);
+
+        model.addAttribute("message", message);
+
+        return "dice-roll-results";
     }
-
-
 }
+
+
+
